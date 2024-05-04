7f1dded507207e1b2bddcc68bcf7c73e84420aa1
sergei <solomatovs@gmail.com>
2024-05-04 10:52:32 +0300

    dyd init

---
+++ examples/my_struct.rs
@@ -0,0 +1,21 @@
+#![feature(macro_metavar_expr)]
+#![feature(trace_macros)]
+
+
+pub use dyd::dyd;
+
+fn main() {
+    trace_macros!(true);
+
+    dyd! (
+        #[derive(Copy, Clone, Debug)]
+        pub struct MyStruct {
+            pub fn fn_one(self, one: u8),
+            fn fn_two(&self, one: u8),
+            fn fn_tree(&mut self, one: u8),
+            fn fn_four(one: u8),
+            fn fn_five(one: &mut u8) -> Self,
+            one: u8,
+        }
+    );
+}
---
+++ src/lib.rs
@@ -0,0 +1,201 @@
+#![feature(trace_macros)]
+
+// use libloading::{Error, Library, Symbol};
+// use std::path::{Path, PathBuf};
+// use std::ffi::{OsStr, OsString};
+// use std::sync::Arc;
+
+
+// use std::sync::RwLock;
+// use std::fmt::Debug;
+use std::sync::TryLockError;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum DydError {
+    #[error("failed to take the lock {0}, try again later")]
+    WouldBlock(String),
+
+    #[error("poisoned lock {0}. please reload lib {0}")]
+    PoisonedLock(String, String),
+
+    // #[error("failed to load {0}: {1}")]
+    // FailedToLoadLib(String, libloading::Error),
+
+    // #[error("failed to load symbol {0} in lib {1}: {2}")]
+    // FailedToLoadSymbol(String, String, libloading::Error),
+    #[error("failed to load {0}")]
+    FailedToLoadLib(libloading::Error),
+
+    #[error("failed to load symbol {0}")]
+    FailedToLoadSymbol(libloading::Error),
+}
+
+impl From<libloading::Error> for DydError {
+    fn from(item: libloading::Error) -> Self {
+        DydError::FailedToLoadLib(item)
+    }
+}
+
+impl<T> From<TryLockError<T>> for DydError {
+    fn from(item: TryLockError<T>) -> Self {
+        match item {
+            TryLockError::WouldBlock => DydError::WouldBlock("lib".into()),
+            TryLockError::Poisoned(x) => DydError::PoisonedLock(x.to_string(), "lib".into()),
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! as_item( ($i:item) => ($i) );
+
+#[macro_export]
+macro_rules! dyd(
+    (
+        $(#[$attr:meta])*
+        $vis:vis $(<$($lifetime:lifetime),+>)*
+        struct $name:ident $body:tt
+    ) => (
+        dyd!(parse [$(#[$attr])* $vis $(<$($lifetime),+>)* struct $name] [impl $name] [] [] [] $body);
+    );
+    
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {}
+    ) => (
+        dyd!(output $decl $impl
+            [$($member)*]
+            [$($fn)*]
+            [$($fn_member)*]
+        );
+    );
+
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            $(#[$attr:meta])*
+            $vis:vis $(<$($lifetime:lifetime),+>)*
+            fn $name:ident(&mut self $(,)? $($item:ident:$ty:ty),*) $(-> $ret:ty)?, $($t:tt)*
+        }
+    ) => (
+        dyd!(parse $decl $impl 
+            [$($member)*]
+            [$($fn)*]
+            [$($fn_member)*]
+            {
+                [$(#[$attr])* $vis $(<$($lifetime),+>)*],
+                [fn $name], 
+                [self], [&mut Self,], [&mut self,], [self,], 
+                [$($item:$ty),*], [$(-> $ret)?], $($t)*
+            }
+
+        );
+    );
+
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            $(#[$attr:meta])*
+            $vis:vis $(<$($lifetime:lifetime),+>)*
+            fn $name:ident(&self $(,$item:ident:$ty:ty)*) $(-> $ret:ty)?, $($t:tt)*
+        }
+    ) => (
+        dyd!( parse $decl $impl 
+            [$($member)*]
+            [$($fn)*]
+            [$($fn_member)*]
+            {
+                [$(#[$attr])* $vis $(<$($lifetime),+>)*],
+                [fn $name],
+                [self], [&Self,], [&self,], [self,],
+                [$($item:$ty),*], [$(-> $ret)?], $($t)*
+            }
+
+        );
+    );
+
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            $(#[$attr:meta])*
+            $vis:vis $(<$($lifetime:lifetime),+>)*
+            fn $name:ident(self $(,$item:ident:$ty:ty)*) $(-> $ret:ty)?, $($t:tt)*
+        }
+    ) => (
+        dyd!( parse $decl $impl 
+            [$($member)*]
+            [$($fn)*]
+            [$($fn_member)*]
+            {
+                [$(#[$attr])* $vis $(<$($lifetime),+>)*],
+                [fn $name],
+                [self], [Self,], [self,], [self,],
+                [$($item:$ty),*], [$(-> $ret)?], $($t)*
+            }
+
+        );
+    );
+    
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            $(#[$attr:meta])*
+            $vis:vis $(<$($lifetime:lifetime),+>)*
+            fn $name:ident($($item:ident:$ty:ty)*) $(-> $ret:ty)?, $($t:tt)*
+        }
+    ) => (
+        dyd!( parse $decl $impl 
+            [$($member)*]
+            [$($fn)*]
+            [$($fn_member)*]
+            {
+                [$(#[$attr])* $vis $(<$($lifetime),+>)*],
+                [fn $name],
+                [self], [], [&self,], [],
+                [$($item:$ty),*], [$(-> $ret)?], $($t)*
+            }
+
+        );
+    );
+    
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            [$fnattr:tt], [fn $name:ident], [$self:tt], [$($member_self:tt)*], [$($fn_decl_self:tt)*], [$($fn_call_self:tt)*], [$($item:ident : $ty:ty),*], [$(-> $ret:ty)?], $($t:tt)*
+        }
+    ) => (
+        dyd!(parse $decl $impl
+            [$($member)* $name: fn($($member_self)* $($ty),*) $(-> $ret)?,]
+            [$($fn)* $fnattr fn $name($($fn_decl_self)* $($item:$ty),*) $(-> $ret)? {
+                ($self.$name)($($fn_call_self)* $($item),*)
+            }]
+            [ $($fn_member)*, ]
+            { $($t)* }
+        );
+    );
+    
+    (
+        parse $decl:tt $impl:tt [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*] {
+            $name:ident: $typ:ty, $($t:tt)*
+        }
+    ) => (
+        dyd!(parse $decl $impl 
+            [$($member)* $name: $typ,]
+            [$($fn)*]
+            [$($fn_member)*]
+            { $($t)* }
+        );
+    );
+
+    (
+        output [$($decls:tt)*] [$($impl:tt)*] [$($member:tt)*] [$($fn:tt)*] [$($fn_member:tt)*]
+    )
+    => (
+        $crate::as_item!(
+            $($decls)* {
+                $($member)*
+            }
+        );
+
+        $crate::as_item!(
+            $($impl)* {
+                $($fn)*
+            }
+        );
+    );
+);
